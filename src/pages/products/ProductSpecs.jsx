9b6f83eb2c72bacc068ee7bccaa661bd167b0800
Willummp <lsousaah@gmail.com>
2023-09-28 15:25:21 -0300

    fixed image

--- src/pages/products/ProductSpecs.jsx
+++ src/pages/products/ProductSpecs.jsx
@@ -8,7 +8,7 @@ function ProductSpecs({ route }) {
   return (
     <View>
      
-      <Image source={{ uri: product.image }} style={{ width: "100%", height: 600, alignSelf:"center" }} />
+      <Image source={{ uri: product.image }} style={{ width: "100%", height: '70%', alignSelf:"center",   resizeMode: 'contain' }} />
       <Text style={{alignSelf:"center"}}>Especificações Detalhadas:</Text>
       <Text style={{ width: "80%", fontSize: 16, alignSelf:"center" }} >{specifications}</Text>
     </View>
